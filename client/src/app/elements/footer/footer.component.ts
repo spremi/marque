381a84f552532a0c52ba240d2faa56e4afe62795
Sanjeev Premi <spremi@ymail.com>
2018-09-16 02:34:51 +0530

    c) feat: footer: Show locally hosted license text
    Instead of opening external link in new window...
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/elements/footer/footer.component.ts
+++ client/src/app/elements/footer/footer.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HintService } from '../../services/hint.service';
+import { LicenseService } from '../../services/license.service';
+
 @Component({
   selector: 'sp-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.sass']
+  styleUrls: ['./footer.component.sass'],
 })
 export class FooterComponent implements OnInit {
+  constructor(
+    private hintSvc: HintService,
+    private licenseSvc: LicenseService,
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLicenseHover() {
+    this.hintSvc.show('The 3-Clause BSD License. Click to view.');
   }
 
+  onLicenseOut() {
+    this.hintSvc.clear();
+  }
+
+  onLicenseClick() {
+    this.licenseSvc.show();
+  }
 }
