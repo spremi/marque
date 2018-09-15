449fff2ca9023506d6496f29240bf2d62b01697e
Sanjeev Premi <spremi@ymail.com>
2018-09-15 23:34:12 +0530

    c) feat: interface: Define 'Icon'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/models/icon.ts
+++ client/src/app/models/icon.ts
@@ -1,2 +1,10 @@
+/**
+ * Maps an icon name to a url
+ *
+ * @property name   Icon name
+ * @property file   Link to corresponding icon
+ */
 export interface Icon {
+  name: string;
+  link: string;
 }
