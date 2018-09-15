86143e4bd486c272cb1e6c82ca558feb575178a2
Sanjeev Premi <spremi@ymail.com>
2018-09-16 01:56:19 +0530

    c) feat: Add class AppConst
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/app.constants.ts
@@ -0,0 +1,43 @@
+/**
+ * @file        [marque] client/src/app/app.constants.ts
+ *
+ * @copyright   Sanjeev Premi, 2018.
+ *
+ * @license     BSD-3-Clause
+ */
+
+/**
+ * Encapsulates commonly used constants.
+ * @class
+ */
+export class AppConst {
+  /**
+   * Base URL for REST api.
+   * @constant
+   */
+  static readonly API_BASE = 'http://localhost:4900/api/';
+
+  /**
+   * Token for 'category' resource in REST api.
+   * @constant
+   */
+  static readonly RES_CATEGORY = 'category';
+
+  /**
+   * Token for 'bookmark' resource in REST api.
+   * @constant
+   */
+  static readonly RES_BOOKMARK = 'bookmark';
+
+  /**
+   * Token for 'icon' resource in REST api.
+   * @constant
+   */
+  static readonly RES_ICON = 'icon';
+
+  /**
+   * Token for 'license' resource in REST api.
+   * @constant
+   */
+  static readonly RES_LICENSE = 'license';
+}
