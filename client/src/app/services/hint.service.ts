04ae729520d516fa8b51a743e8e4b6b777cb3021
Sanjeev Premi <spremi@ymail.com>
2018-09-16 01:28:41 +0530

    c) feat: hint-service: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/hint.service.ts
+++ client/src/app/services/hint.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HintService {
+  private _hint = new BehaviorSubject<string>(null);
 
-  constructor() { }
+  public hint = this._hint.asObservable();
+
+  constructor() {}
+
+  show(arg: string) {
+    this._hint.next(arg);
+  }
+
+  clear() {
+    this._hint.next(null);
+  }
 }
