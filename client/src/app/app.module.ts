0e1fadf7b357c2e01d81adae4d59b3ab94f88cd4
Sanjeev Premi <spremi@ymail.com>
2018-09-15 23:38:19 +0530

    c) chore: Add Data service
    Command:
      ng g s services/data
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
