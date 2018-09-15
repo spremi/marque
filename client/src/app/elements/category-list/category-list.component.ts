820bce14e47f73da45d80cc37bf7687f4cfdd027
Sanjeev Premi <spremi@ymail.com>
2018-09-16 03:02:34 +0530

    c) feat: category-list: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/elements/category-list/category-list.component.ts
+++ client/src/app/elements/category-list/category-list.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { forkJoin, Observable, Subscription } from 'rxjs';
+import { AppConst } from 'src/app/app.constants';
+import { Icon } from 'src/app/models/icon';
+
+import { Category } from '../../models/category';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'sp-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.sass']
+  styleUrls: ['./category-list.component.sass'],
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent implements OnInit, OnDestroy {
+  /**
+   * List of categories
+   */
+  list: Category[] = null;
 
-  constructor() { }
+  /**
+   * List of icons
+   */
+  icons: Icon[] = null;
+
+  private subData: Subscription = null;
+
+  constructor(private dataSvc: DataService) {}
 
   ngOnInit() {
+    const obsIcons: Observable<Icon[]> = this.dataSvc.getIcons();
+    const obsCategories: Observable<Category[]> = this.dataSvc.getCategories();
+
+    this.subData = forkJoin(obsIcons, obsCategories).subscribe(
+      ([icons, categories]) => {
+        this.icons = icons;
+
+        //
+        // Replace icon 'id' with icon 'file'
+        //
+        categories.forEach((cat) => {
+          cat.icon = this.iconFile(cat.icon);
+        });
+
+        this.list = categories;
+      },
+    );
   }
 
+  ngOnDestroy() {
+    if (this.subData) {
+      this.subData.unsubscribe();
+    }
+  }
+
+  /**
+   * Find link corresponding to specified icon name.
+   *
+   * @param name  Name of the icon
+   */
+  private iconFile(name: string): string {
+    if (Array.isArray(this.icons)) {
+      const icon: Icon = this.icons.find((elem) => elem.name === name);
+
+      if (icon) {
+        return icon.link;
+      }
+    }
+
+    return AppConst.DEFAULT_ICON;
+  }
 }
