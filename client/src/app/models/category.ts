485ef420db2865fef7fb098fea414d56f0f2a38b
Sanjeev Premi <spremi@ymail.com>
2018-09-15 23:30:38 +0530

    c) feat: interface: Define 'Category'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/models/category.ts
+++ client/src/app/models/category.ts
@@ -1,2 +1,14 @@
+/**
+ * Describes a category
+ *
+ * @property id     Identifier
+ * @property name   Name of the category
+ * @property desc   Brief description of category
+ * @property icon   Associated icon
+ */
 export interface Category {
+  id: string;
+  name: string;
+  desc: string;
+  icon: string;
 }
