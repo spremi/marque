710dae2bad48f7488eca593e623bae28964b6bbc
Sanjeev Premi <spremi@ymail.com>
2018-09-16 02:07:48 +0530

    c) feat: license-svc: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/services/license.service.ts
+++ client/src/app/services/license.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { AppConst } from '../app.constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LicenseService {
+  private _trigger = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  public trigger = this._trigger.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  show() {
+    this._trigger.next(true);
+  }
+
+  hide() {
+    this._trigger.next(false);
+  }
+
+  /**
+   * Get license text.
+   */
+  text(): Observable<string> {
+    const link = AppConst.API_BASE + AppConst.RES_LICENSE;
+
+    return this.http.get(link, { responseType: 'text' });
+  }
 }
