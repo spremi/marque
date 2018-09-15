a399f2d9e9432a58b0592d38063058c3887d7490
Sanjeev Premi <spremi@ymail.com>
2018-09-15 23:45:22 +0530

    c) chore: Add Header component
    Command:
      ng g c elements/header
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './elements/header/header.component';
 import { DataService } from './services/data.service';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [DataService],
   bootstrap: [AppComponent],
