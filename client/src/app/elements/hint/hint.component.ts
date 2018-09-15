ba2ad14d3f83bf7f672b1963bafb494ab1987247
Sanjeev Premi <spremi@ymail.com>
2018-09-16 01:37:09 +0530

    c) feat: hint: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/elements/hint/hint.component.ts
+++ client/src/app/elements/hint/hint.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { HintService } from '../../services/hint.service';
 
 @Component({
   selector: 'sp-hint',
   templateUrl: './hint.component.html',
-  styleUrls: ['./hint.component.sass']
+  styleUrls: ['./hint.component.sass'],
 })
-export class HintComponent implements OnInit {
+export class HintComponent implements OnInit, OnDestroy {
+  private subHint: Subscription = null;
 
-  constructor() { }
+  hint: string = null;
+
+  constructor(private hintSvc: HintService) {}
 
   ngOnInit() {
+    this.subHint = this.hintSvc.hint.subscribe((arg: string) => {
+      this.hint = arg;
+    });
   }
 
+  ngOnDestroy() {
+    if (this.subHint) {
+      this.subHint.unsubscribe();
+    }
+  }
 }
