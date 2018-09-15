0644ee09804309e48f93600ef137e50b52b8be55
Sanjeev Premi <spremi@ymail.com>
2018-09-15 23:47:15 +0530

    c) chore: Add Footer component
    Command:
      ng g c elements/footer
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './elements/footer/footer.component';
 import { HeaderComponent } from './elements/header/header.component';
 import { DataService } from './services/data.service';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [DataService],
   bootstrap: [AppComponent],
