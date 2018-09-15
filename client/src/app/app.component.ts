e38a4394b5b7ddc4c9988daf00db8b440760e796
Sanjeev Premi <spremi@ymail.com>
2018-09-16 02:32:03 +0530

    c) feat: app: Add license component
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { LicenseService } from './services/license.service';
 
 @Component({
   selector: 'sp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
-  title = 'marqe-client';
+export class AppComponent implements OnInit, OnDestroy {
+  private subTrigger: Subscription = null;
+
+  /**
+   * Flag - Show/ Hide the license.
+   */
+  isLicense = false;
+
+  constructor(private licenseSvc: LicenseService) {}
+
+  ngOnInit() {
+    this.subTrigger = this.licenseSvc.trigger.subscribe((arg: boolean) => {
+      this.isLicense = arg;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subTrigger) {
+      this.subTrigger.unsubscribe();
+    }
+  }
 }
