82b496dc19daacbca625fd187f6734354120c8a6
Sanjeev Premi <spremi@ymail.com>
2018-09-16 02:48:16 +0530

    c) feat: category: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/elements/category/category.component.ts
+++ client/src/app/elements/category/category.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Category } from '../../models/category';
+import { HintService } from '../../services/hint.service';
+import { SelectService } from '../../services/select.service';
 
 @Component({
   selector: 'sp-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.sass']
+  styleUrls: ['./category.component.sass'],
 })
-export class CategoryComponent implements OnInit {
+export class CategoryComponent implements OnInit, OnDestroy {
+  private subSelect: Subscription = null;
 
-  constructor() { }
+  @Input()
+  cat: Category;
+
+  @HostBinding('class.selected')
+  selected = false;
+
+  constructor(private selectSvc: SelectService, private hintSvc: HintService) {}
 
   ngOnInit() {
+    this.subSelect = this.selectSvc.category.subscribe((arg: Category) => {
+      if (arg !== null) {
+        this.selected = this.cat.id === arg.id;
+      }
+    });
   }
 
+  ngOnDestroy() {
+    if (this.subSelect) {
+      this.subSelect.unsubscribe();
+    }
+  }
+
+  onSelect() {
+    this.selectSvc.set(this.cat);
+  }
+
+  @HostListener('mouseover')
+  onHover() {
+    this.hintSvc.show(this.cat.desc);
+  }
+
+  @HostListener('mouseout')
+  offHover() {
+    this.hintSvc.clear();
+  }
 }
