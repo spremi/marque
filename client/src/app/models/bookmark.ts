042afb185d996bf7ce383a52113a2669094ced6a
Sanjeev Premi <spremi@ymail.com>
2018-09-15 23:32:50 +0530

    c) feat: interface: Define 'Bookmark'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/models/bookmark.ts
+++ client/src/app/models/bookmark.ts
@@ -1,2 +1,14 @@
+/**
+ * Describes a bookmark
+ *
+ * @property id     Identifier
+ * @property link   URL
+ * @property name   Name of the bookmark
+ * @property desc   Brief description of bookmark
+ */
 export interface Bookmark {
+  id: string;
+  link: string;
+  name: string;
+  desc: string;
 }
