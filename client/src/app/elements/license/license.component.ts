42eb66d7d962278e54c9d4a44ea1bdbf5dd83e5d
Sanjeev Premi <spremi@ymail.com>
2018-09-16 02:24:56 +0530

    c) feat: license: Initial implementation
    The classes contained in the innerHTML are, by default,
    ignored by Angular during rendering.
    
    However, the global style definitions still apply.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/elements/license/license.component.ts
+++ client/src/app/elements/license/license.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
+
+import { LicenseService } from '../../services/license.service';
 
 @Component({
   selector: 'sp-license',
   templateUrl: './license.component.html',
-  styleUrls: ['./license.component.sass']
+  styleUrls: ['./license.component.sass'],
 })
-export class LicenseComponent implements OnInit {
+export class LicenseComponent implements OnInit, OnDestroy {
+  private subLicense: Subscription = null;
 
-  constructor() { }
+  text: SafeHtml = null;
+
+  constructor(
+    private licenseSvc: LicenseService,
+    private sanitizer: DomSanitizer,
+  ) {}
 
   ngOnInit() {
+    this.subLicense = this.licenseSvc.text().subscribe((result: string) => {
+      this.text = this.sanitizer.bypassSecurityTrustHtml(result);
+    });
   }
 
+  ngOnDestroy() {
+    if (this.subLicense) {
+      this.subLicense.unsubscribe();
+    }
+  }
+
+  onClose() {
+    this.licenseSvc.hide();
+  }
 }
