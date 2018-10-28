cf81dcd0b310421234c4a511ec2df0f6c9aeb0eb
Sanjeev Premi <spremi@ymail.com>
2018-10-28 22:07:15 +0530

    c) chore: Fix typo in the name
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to marqe-client!');
+    expect(page.getParagraphText()).toEqual('Welcome to marque-client!');
   });
 });
--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'marqe-client'`, async(() => {
+  it(`should have as title 'marque-client'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('marqe-client');
+    expect(app.title).toEqual('marque-client');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to marqe-client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to marque-client!');
   }));
 });
